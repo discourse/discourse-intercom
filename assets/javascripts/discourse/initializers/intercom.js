4a3ad9c8753bee4d90149f6e00a6f6f3e1a60ee6
Jarek Radosz <jradosz@gmail.com>
2024-01-16 01:33:09 +0100

    DEV: Update linting (#17)

---
+++ assets/javascripts/discourse/initializers/intercom.js
@@ -0,0 +1,29 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+function initialize(api) {
+  const siteSettings = api.container.lookup("service:site-settings");
+  const currentUser = api.getCurrentUser();
+  if (!siteSettings.intercom_enabled || !currentUser) {
+    return;
+  }
+
+  // prettier-ignore
+  // eslint-disable-next-line
+  const startMessenger = () => { let w = window; let ic = w.Intercom; if (typeof ic === "function") { ic('reattach_activator'); ic('update', intercomSettings); } else { let d = document; let i = function () { i.c(arguments); }; i.q = []; i.c = function (args) { i.q.push(args); }; w.Intercom = i; function l() { let s = d.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = `https://widget.intercom.io/widget/${ siteSettings.intercom_app_id }`; let x = d.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); } l(); } };
+
+  window.intercomSettings = {
+    app_id: siteSettings.intercom_app_id,
+    email: currentUser.email,
+    user_hash: currentUser.intercom_hash,
+  };
+
+  startMessenger();
+}
+
+export default {
+  name: "intercom",
+
+  initialize() {
+    withPluginApi("0.8.7", initialize);
+  },
+};
--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
 # name: discourse-intercom
-# about: Adds Intercom Messenger Integration to Discourse. 
+# about: Adds Intercom Messenger Integration to Discourse.
 # meta_topic_id: 106622
 # version: 0.1
 # author: Vinoth Kannan
@@ -7,15 +8,9 @@
 enabled_site_setting :intercom_enabled
 
 after_initialize do
-  add_to_serializer(:current_user, :email) do
-    object.email
-  end
+  add_to_serializer(:current_user, :email) { object.email }
 
   add_to_serializer(:current_user, :intercom_hash) do
-    OpenSSL::HMAC.hexdigest(
-      'sha256',
-      SiteSetting.intercom_secret_key,
-      object.email
-    )
+    OpenSSL::HMAC.hexdigest("sha256", SiteSetting.intercom_secret_key, object.email)
   end
 end
